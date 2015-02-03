654ea0fa364a2b264fa26774b6051cfd8090d560
Hao <fudhao@gmail.com>
2015-02-03 10:39:34 +0800

    make a gem to share.

---
+++ codes21.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'codes21'
+  s.version     = '0.0.1'
+  s.licenses    = ['MIT']
+  s.date        = '2015-02-03'
+  s.summary     = "Codes to One."
+  s.description = "With the given suffixes merge multiple files code into one file."
+  s.authors     = ["Aston Fu"]
+  s.email       = 'im@fuhao.im'
+  s.files       = ["lib/codes21.rb"]
+  s.executables   = %w(codes21)
+  s.homepage    = 'https://github.com/astonfu/codes21'
+end
---
+++ lib/codes21.rb
@@ -0,0 +1,37 @@
+class Codes21
+  def get_codes(path, suffixes)
+    @items ||= []
+    Dir.glob("#{path}/**/*.{#{suffixes}}").each do |file|
+      title = file.split('/').last
+      puts file
+
+      puts '*' * 70
+      codes = File.read(file)
+      codes.squeeze! "\n"
+      puts codes
+    
+      item = {title: title, codes: codes}
+      @items << item
+    end
+  end
+  
+  def write_result(result_file_path)
+    File.open(result_file_path, 'w') do |f|
+      @items.each do |item|
+        f.puts "File:" + item[:title]
+        f.puts "Code:"
+        f.puts item[:codes]
+      end
+    end
+
+    puts '*' * 70
+    puts "codes files count:  #{@items.size}"
+  end
+
+  def self.run(path, suffixes, result_file_path)
+    codes21 = Codes21.new
+    codes21.get_codes(path, suffixes)
+    codes21.write_result(result_file_path)
+  end
+
+end
