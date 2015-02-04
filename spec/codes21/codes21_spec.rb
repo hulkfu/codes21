9131049b680738bf704803b5f3fd03003a583cb5
Hao <fudhao@gmail.com>
2015-02-04 11:32:11 +0800

    add spec test.

---
+++ spec/codes21/codes21_spec.rb
@@ -0,0 +1,12 @@
+require 'codes21'
+
+describe Codes21 do
+  after { File.delete "result.txt" if File.exists?("result.txt") }
+
+  it "merge codes into one txt file" do
+    Codes21.run ".", "rb", "result.txt"
+    expect(File.exists?("result.txt")).to eq(true)
+    
+  end
+
+end
